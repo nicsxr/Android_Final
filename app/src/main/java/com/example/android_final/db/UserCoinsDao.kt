6cb9813bd4c8ff2e8857434312dd6c84e3440907
nicsxr <36042283+nicsxr@users.noreply.github.com>
2022-06-24 17:38:16 +0400

    coins sort

--- app/src/main/java/com/example/android_final/db/UserCoinsDao.kt
+++ app/src/main/java/com/example/android_final/db/UserCoinsDao.kt
@@ -8,7 +8,7 @@ interface UserCoinsDao {
     @Query("Select * from UserCoins")
     fun getAllData() : MutableList<UserCoins>
 
-    @Query("Select coinId, name, symbol, buyAmount as totalBuy, coinAmount, buyPrice as averagePrice, buyDate, imageUrl as image from UserCoins WHERE coinId LIKE :coinId")
+    @Query("Select coinId, name, symbol, buyAmount as totalBuy, coinAmount, buyPrice as averagePrice, buyDate, imageUrl as image from UserCoins WHERE coinId LIKE :coinId ORDER BY buyDate desc")
     fun getCoinData(coinId : String) : MutableList<AverageCoins>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
@@ -17,6 +17,6 @@ interface UserCoinsDao {
     @Query("DELETE FROM UserCoins")
     fun deleteData()
 
-    @Query("Select coinId, name, symbol, SUM(buyAmount) as totalBuy, SUM(coinAmount) as coinAmount, AVG(buyPrice) as averagePrice, MAX(buyDate), imageUrl as image from UserCoins Group by coinId")
+    @Query("Select coinId, name, symbol, SUM(buyAmount) as totalBuy, SUM(coinAmount) as coinAmount, AVG(buyPrice) as averagePrice, MAX(buyDate), imageUrl as image from UserCoins Group by coinId ORDER BY totalBuy desc")
     fun getAverageData() : List<AverageCoins>
 }
